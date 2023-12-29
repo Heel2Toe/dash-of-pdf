78ca339b9999291c300d01645ad8f6ce37e02a95
unknown <you@example.com>
2023-12-29 16:28:45 +0530

    visual: new login page

--- components/login.tsx
+++ components/login.tsx
@@ -1,49 +1,57 @@
-'use client'
-import { useUser } from '@/hooks/useUser'
+"use client";
+import { useUser } from "@/hooks/useUser";
 import { loginUser } from "@/actions/loginUser";
-import { useRouter } from 'next/navigation';
-import toast from 'react-hot-toast';
-import { useEffect } from 'react';
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import { useEffect, useState } from "react";
+import Button from "./ui/button";
+import classNames from "classnames";
 
 const Login = () => {
-   
-  const {updateUser, email} = useUser();  
+  const { updateUser, email } = useUser();
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
   const router = useRouter();
 
   const logIn = async () => {
-  try{
-    const result = await loginUser(updateUser);
-    if(result == 'ok'){
-        router.push('/dash');
+    try {
+      setLoading(true);
+      const result = await loginUser(updateUser);
+      if (result == "ok") {
+        setSuccess(true);
+        router.push("/dash");
+      }
+    } catch (err) {
+      toast.error("Something went wrong");
+      console.log(err);
     }
- } catch(err){
-    toast.error('Something went wrong');
-    console.log(err);
-  }
-}
+    finally{
+      setLoading(false)
+    }
+  };
 
-useEffect(()=>{
-  if(email != ''){
-    toast('Welcome Back', {icon: '🙌'})
-    router.push('/dash');
-  }
-},[])
+  useEffect(() => {
+    if (email != "") {
+      toast("Welcome Back", { icon: "🙌" });
+      router.push("/dash");
+    }
+  }, []);
 
+  return (
+    <div className="h-full flex flex-col space-y-4 p-32 items-center">
 
-  return ( 
-    <div className="h-full bg-red-600 flex flex-col p-32 items-center">
-
-      <div className="flex flex-col items-center justify-center w-40">
-        <h1 className=" text-3xl">Dash of</h1>
-      <img src="./images/pdfLogo.png" alt="" className="h-40 w-40 bg-white" />
+      <div onClick={logIn} className={classNames(`border rounded-xl flex flex-col space-y-5 items-center justify-center p-8
+        cursor-pointer hover:-translate-y-3 hover:border-red-500 transition duration-500`,
+         success ? 'bg-green-400' : loading && 'bg-orange-400'
+        )}>
+        <div className="flex flex-col items-center justify-center">
+          <h1 className=" text-3xl">Dash of</h1>
+          <img src="./images/pdfLogo.png" alt="" className="h-40 w-40" />
+        </div>
+        <h1 className="text-gray-600">Click to get started !</h1>
       </div>
-
-      <button onClick={logIn}>
-         get started !
-      </button>
-
     </div>
-   );
-}
- 
+  );
+};
+
 export default Login;
